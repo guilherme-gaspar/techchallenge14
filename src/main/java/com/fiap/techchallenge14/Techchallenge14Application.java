2211c997a0835cbda33c669bccbeba58dbac0043
Guilherme Gaspar <guilherme.c.gaspargg@gmail.com>
2025-10-04 15:03:55 -0300

    First commit

---
+++ src/main/java/com/fiap/techchallenge14/Techchallenge14Application.java
@@ -0,0 +1,13 @@
+package com.fiap.techchallenge14;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Techchallenge14Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Techchallenge14Application.class, args);
+    }
+
+}
