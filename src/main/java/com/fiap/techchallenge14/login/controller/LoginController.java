d6d7965c544e93f945ac76cf4d3c139e11b231ae
Guilherme Gaspar <guilherme.c.gaspargg@gmail.com>
2025-10-06 20:35:55 -0300

    feat: add user and login controllers

---
+++ src/main/java/com/fiap/techchallenge14/login/controller/LoginController.java
@@ -0,0 +1,25 @@
+package com.fiap.techchallenge14.login.controller;
+
+import com.fiap.techchallenge14.login.dto.LoginRequestDTO;
+import com.fiap.techchallenge14.login.service.LoginService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/login")
+@RequiredArgsConstructor
+public class LoginController {
+
+    private final LoginService loginService;
+
+    @PostMapping
+    public ResponseEntity<String> login(@RequestBody LoginRequestDTO loginRequest) {
+        String token = loginService.login(loginRequest);
+        return ResponseEntity.ok(token);
+    }
+}
+
